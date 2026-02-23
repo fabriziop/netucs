dfc56fd8ed96aad1714476f8e93cddc17cea607c
Fabrizio Pollastri <mxgbot@gmail.com>
2026-02-23 21:25:19 +0100

    Add package configuration files: setup.py, LICENSE, pyproject.toml, MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Setup configuration for netucs package
+"""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="netucs",
+    version="1.0.0",
+    author="Fabrizio Pollastri",
+    author_email="f.pollastri@inrim.it",
+    description="Network UDP Client Server data exchange - asynchronous client/server UDP communication",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/f-pollastri/netucs",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Communications",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: Other/Proprietary License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    keywords="udp client server asyncio network communication data-exchange",
+    project_urls={
+        "Bug Reports": "https://github.com/f-pollastri/netucs/issues",
+        "Source": "https://github.com/f-pollastri/netucs",
+    },
+)
